eee2026773cce90f09295b316b79cfdc74b07d0d
Lznah <hanzlpe@icloud.com>
2018-07-23 12:50:27 +0200

    Added portfolio page and edited header template

--- server.js
+++ server.js
@@ -49,11 +49,17 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pagetitle : 'About Page',
+  });
+});
+
 // /bad
 
 app.get('/bad', (req, res) => {
   res.send({
-    erroMessage: 'Error'
+    errorMessage: 'Error'
   });
 });
 
